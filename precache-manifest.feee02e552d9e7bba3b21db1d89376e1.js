b24f2610d0f423219f364bf5b10185babcda72d7
Brian Hunley <brian.hunley@gmail.com>
2020-03-01 17:10:49 -0500

    Updates

---
+++ precache-manifest.feee02e552d9e7bba3b21db1d89376e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d06f4071518a7c53720082d3cc813e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba097cdcfe81979b14b4",
+    "url": "/monsters-rolodex/static/css/main.1d3a4550.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba097cdcfe81979b14b4",
+    "url": "/monsters-rolodex/static/js/main.2f34cb84.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
